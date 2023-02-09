737e0cf1d54a463b0dfd496a819b6d8ec5d76337
technoblade123456 <124906007+technoblade123456@users.noreply.github.com>
2023-02-09 17:50:24 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+num1= int(input("Enter the first number \n"))
+num2= int(input("Enter the second number \n"))
+op= input("Enter the operation \n")
+if op== "+":
+    print(num1+num2)
+elif op== "-":
+    print(num1-num2)
+elif op== "*":
+    print(num1*num2)
+elif op== "/":
+    print (int(num1/num2))
